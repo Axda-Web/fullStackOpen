3e12f0b77d3433913118ef87d4ba8fcb67057c25
Alyx DARENNE <alyx.darenne@deptagency.com>
2023-03-02 16:56:31 +0100

    part2/ex2: Add Total component

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({course: { name, parts}}) => {
   return (
     <>
         <Header name={name} />
         <Content parts={parts} />
+        <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </>
   )
 }
---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = ({sum}) => {
+  return (
+    <p style={{fontWeight: 'bold'}}>Total of {sum} exercices</p>
+  )
+}
+
+export default Total
