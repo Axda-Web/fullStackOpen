265e2919b6b194022884c0e397029ed265a7ffc8
Alyx DARENNE <alyx.darenne@deptagency.com>
2023-03-02 08:36:41 +0100

    part1/ex11: Display data into table layout

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,10 +6,13 @@ const Button = ({ handleClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-      {text === "positive" && "%"}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>
+        {value}
+        {text === "positive" && "%"}
+      </td>
+    </tr>
   );
 };
 
@@ -21,14 +24,16 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
       {all === 0 ? (
         <div>No feeback given</div>
       ) : (
-        <>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={all} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positive} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positive} />
+          </tbody>
+        </table>
       )}
     </>
   );
