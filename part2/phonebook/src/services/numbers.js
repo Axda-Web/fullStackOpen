50b3db2c86a55efecb824561e298ef9384d14607
Alyx DARENNE <alyx.darenne@deptagency.com>
2023-03-04 10:24:04 +0100

    part2/ex13: Create API module

---
+++ part2/phonebook/src/services/numbers.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(BASE_URL)
+    return request.then(response => response.data)
+}
+
+const create = newNumber => {
+    const request = axios.post(BASE_URL, newNumber)
+    return request.then(response => response.data)
+}
+
+export default {
+    getAll,
+    create
+}
